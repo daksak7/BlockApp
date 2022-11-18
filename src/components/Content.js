720a3c11dbfaaca5beea52fdbc507c6e5d149b45
daksak <dogancan_aksak@hotmail.com>
2022-11-18 03:13:58 +0300

    layouts created

---
+++ src/components/Content.js
@@ -0,0 +1,107 @@
+import React from "react";
+
+function Content() {
+  return (
+    <div className="col-md-10 ms-sm-auto col-lg-10 px-md-4 content-container">
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 className="h2">Kullanıcı Paneli</h1>
+      </div>
+      <div className="row">
+        <div className="col-4 portfolio-card">
+          <div className="card text-bg-primary mb-3 portfolio-item">
+            <div className="card-header">Header</div>
+            <div className="card-body">
+              <h5 className="card-title">Primary card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 portfolio-card">
+          <div className="card text-bg-warning mb-3 portfolio-item">
+            <div className="card-header">BINANCE</div>
+            <div className="card-body">
+              <h5 className="card-title">Hesap Detayları:</h5>
+              <p className="card-text">
+                Burada hesap bakiye bilgileri olacak
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 portfolio-card">
+          <div className="card text-bg-success mb-3 portfolio-item">
+            <div className="card-header">Header</div>
+            <div className="card-body">
+              <h5 className="card-title">Primary card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-4 portfolio-card">
+          <div className="card text-bg-dark mb-3 portfolio-item">
+            <div className="card-header">Header</div>
+            <div className="card-body">
+              <h5 className="card-title">Primary card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 portfolio-card">
+          <div className="card text-bg-light mb-3 portfolio-item">
+            <div className="card-header">Header</div>
+            <div className="card-body">
+              <h5 className="card-title">Primary card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 portfolio-card">
+          <div className="card text-bg-info mb-3 portfolio-item">
+            <div className="card-header">Header</div>
+            <div className="card-body">
+              <h5 className="card-title">Primary card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content. Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+                
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      
+  );
+}
+
+export default Content;
